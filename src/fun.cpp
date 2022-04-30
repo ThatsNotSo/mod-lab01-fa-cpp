22f71085480e1429d169cedde15938d3db4d6e6b
ThatsNotSo <104660360+ThatsNotSo@users.noreply.github.com>
2022-04-30 06:13:26 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -31,9 +31,55 @@ unsigned int faStr1(const char *str) {
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+  int countofwords = 0;
+    bool symbol = false;
+    bool number = false;
+    bool firstone = false;
+    bool actuallynotaword = false;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] != ' ') {
+            symbol = true;
+            if (str[i] >= 'A' && str[i] <= 'Z' && !firstone) {
+                firstone = true;
+            }
+            else if (str[i] >= 'A' && str[i] <= 'Z' && firstone) {
+                actuallynotaword = true;
+            }
+            else if ((str[i] < 'a' || str[i] > 'z') && !actuallynotaword) {
+                actuallynotaword = true;
+            }
+        }
+        else if (str[i] == ' ') {
+            if (symbol && firstone && !actuallynotaword) {
+                countofwords++;
+            }
+            actuallynotaword = false;
+            firstone = false;
+            symbol = false;
+        }
+    }
+    return countofwords;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+  bool symbol = false;
+    int countofwords = 0;
+    int wordssum = 0;
+    int wordssuminavg = 0;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] != ' ') {
+            wordssum++;
+            if (!symbol) {
+                symbol = true;
+                countofwords++;
+            }
+        }
+        else if (str[i] == ' ') {
+            if (symbol) {
+                symbol = false;
+            }
+        }
+    }
+    wordssuminavg = round(wordssum / countofwords);
+    return wordssuminavg;
 }
