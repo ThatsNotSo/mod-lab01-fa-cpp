fb16903118b31300878d3ae05d481c1ccb18a91c
ThatsNotSo <104660360+ThatsNotSo@users.noreply.github.com>
2022-04-30 10:00:52 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -11,15 +11,13 @@ unsigned int faStr1(const char *str) {
     for (int i = 0; i < strlen(str); i++) {
         if (str[i] != ' ' && !symbol) {
             symbol = true;
-        }
-        else if (str[i] >= '0' && str[i] <= '9') {
+        } else if (str[i] >= '0' && str[i] <= '9') {
             number = true;
         }
         if (str[i] == ' ' && symbol && !number) {
             symbol = false;
             countofwords++;
-        }
-        else if (str[i] == ' ' && symbol) {
+        } else if (str[i] == ' ' && symbol) {
             symbol = false;
             number = false;
         }
@@ -41,15 +39,12 @@ unsigned int faStr2(const char *str) {
             symbol = true;
             if (str[i] >= 'A' && str[i] <= 'Z' && !firstone) {
                 firstone = true;
-            }
-            else if (str[i] >= 'A' && str[i] <= 'Z' && firstone) {
+            } else if (str[i] >= 'A' && str[i] <= 'Z' && firstone) {
+                actuallynotaword = true;
+            } else if ((str[i] < 'a' || str[i] > 'z') && !actuallynotaword) {
                 actuallynotaword = true;
             }
-            else if ((str[i] < 'a' || str[i] > 'z') && !actuallynotaword) {
-                actuallynotaword = true;
-            }
-        }
-        else if (str[i] == ' ') {
+        } else if (str[i] == ' ') {
             if (symbol && firstone && !actuallynotaword) {
                 countofwords++;
             }
@@ -73,8 +68,7 @@ unsigned int faStr3(const char *str) {
                 symbol = true;
                 countofwords++;
             }
-        }
-        else if (str[i] == ' ') {
+        } else if (str[i] == ' ') {
             if (symbol) {
                 symbol = false;
             }
